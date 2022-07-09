8f17f1f9b70dc4c59dbbf27352222821991e8c41
Venera <osmven@gmail.com>
2022-07-09 20:02:17 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        Class cl = new Class(20,"Venera",new int [] {3,56,90,1});
+        System.out.println(cl.getArray());
+        System.out.println(cl.getNumber());
+        System.out.println(cl.getWord());
+
+
+
+
+    }
+}
